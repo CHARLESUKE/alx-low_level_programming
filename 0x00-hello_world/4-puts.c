acaf46cce84b9598fdbe3c778c84c7727b7e04ee
CHARLESUKE <ukcharleay@gmail.com>
2023-08-10 14:30:01 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function that executes the program
+ *
+ * Return 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle,\n");
+		return (0);
+}
