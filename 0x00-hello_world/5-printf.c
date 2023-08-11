cbbc0fab885ba811109773b86b1f7e4249973a73
CHARLESUKE <ukcharleay@gmail.com>
2023-08-11 04:00:30 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 /**
- * main -entry point
- * Return Always 0 (Sucess)
- */
+ * * main - Entry point
+ * *
+ * * Return: Always 0 (Success)
+ * */
 int main(void)
 {
-	prinf("with proper grammar, but the outcome is a piece of art,\n"):
-		return (0);
-}	
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
+
