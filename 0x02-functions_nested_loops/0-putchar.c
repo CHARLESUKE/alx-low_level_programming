4202c17cb83310e9f709c29d1a1fec685fb4dbbd
CHARLESUKE <ukcharleay@gmail.com>
2023-08-15 09:07:05 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - function that prints _putchar and
+ * terminates when done
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch > 7; ch++)
+	{
+		_putchar(str[ch]);
+	}
+	_putchar('\n');
+	return (0);
+}
