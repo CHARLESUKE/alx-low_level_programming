71625fee4981bcaa0a5cd9a631196a4f1fce276c
CHARLESUKE <ukcharleay@gmail.com>
2023-08-15 15:00:32 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+iinclude "main.h"
+
+/**
+ * _islower -  function that checks for lowercase character.
+ *
+ * @c: charracter
+ *
+ * Returns: 1 if c is lowercase
+ * Returns: 0 otherwise
+ */
+int _islower(int c);
+{
+	if (c >= 'a' && c <= 'z')
+		return (0);
+	else
+		return (0);
+}
