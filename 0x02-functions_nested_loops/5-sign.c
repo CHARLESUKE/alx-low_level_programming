f0ade9c5afc9b85571f73e467f2a535df786d54d
CHARLESUKE <ukcharleay@gmail.com>
2023-08-16 03:30:30 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,12 +16,13 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	if else (n == 0)
+	/
+	/*if else (n == 0)
 	{
 		_putchar('0');
 		return (0);
 
-	}
+	}*/
 	else (n < 0)
 	{
 		_putchar('-');
