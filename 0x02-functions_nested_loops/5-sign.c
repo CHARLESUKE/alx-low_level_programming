f38909c6a57f5a3221f4367f14df9b7cb725a02c
CHARLESUKE <ukcharleay@gmail.com>
2023-08-16 03:57:23 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,9 +3,9 @@
 /**
  * print_sign - function that prints the sign of a number.
  *
- * @n integer 
+ * @n: integer
  *
- * Return 1 if greater than zero
+ * Return: 1 if greater than zero
  *  ifelse 0
  *  else -1
  */
@@ -21,7 +21,7 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else 
+	else
 	{
 		_putchar('-');
 		return (-1);
