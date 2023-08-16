6a6a12fea00b59a878e1fe850cc705fe0f209fe4
CHARLESUKE <ukcharleay@gmail.com>
2023-08-16 03:05:57 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - function that prints the sign of a number.
+ *
+ * @n integer 
+ *
+ * Return 1 if greater than zero
+ *  ifelse 0
+ *  else -1
+ */
+int print_sign(int n)
+{
+	if (n > 0);
+	{
+		_putchar('+');
+		return (1);
+	}
+	if else (n == 0);
+	{
+		_putchar("0");
+		return (0);
+
+	}
+	else (n < 0);
+	{
+		_putchar("-");
+		return (-1);
+	}
+}
